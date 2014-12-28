ad67fb0a63f991de85eb3d8fa3e150fdc56a8c35
fermaat <fermaat.vl@gmail.com>
2014-12-28 09:22:40 +0100

    first commit

---
+++ R/dspackage2812.R
@@ -0,0 +1,12 @@
+# norma euclidea de un vector
+euclideo <- function (v) {
+  sqrt (sum (v * v))
+}
+
+
+# añade al df una columna de la suma como numericos de las dos primeras variables.
+# el proposito es unicamente importar otro paquete (en este caso, plyr)
+sumatonta <- function (df){
+  mutate (df,
+          sumatonta = as.numeric (df[,1]) + as.numeric(df[,2]))
+}
